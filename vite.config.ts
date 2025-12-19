8cd7e39a23629c162ff5fea9014aa068247fd592
imlearning74 <107246635+imlearning74@users.noreply.github.com>
2025-12-19 09:29:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Il base deve corrispondere al nome della repository su GitHub
+  base: '/Monitoraggio-Costi/',
   plugins: [react()],
   define: {
     'process.env': {
-      API_KEY: process.env.API_KEY,
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+      API_KEY: JSON.stringify(process.env.API_KEY || ''),
+      SUPABASE_URL: JSON.stringify(process.env.SUPABASE_URL || ''),
+      SUPABASE_ANON_KEY: JSON.stringify(process.env.SUPABASE_ANON_KEY || '')
     }
   },
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    emptyOutDir: true,
   }
 });
